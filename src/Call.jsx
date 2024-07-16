4b6908b41a80876e9ebb817927d097377553653f
Feras Muki <feras.muki@gmail.com>
2024-07-16 19:56:52 -0400

    implement phone number formatting

--- src/Call.jsx
+++ src/Call.jsx
@@ -3,7 +3,7 @@ import CallIcon from './CallIcon.jsx';
 import Grid from '@mui/material/Grid';
 
 
-const Call = ({ id, created_at, to, call_type }) => {
+const Call = ({ id, created_at, from, to, call_type }) => {
 
   const getTimeOfDay = (date) => {
     let hour = date.getHours();
@@ -21,18 +21,32 @@ const Call = ({ id, created_at, to, call_type }) => {
     return formatted;
   }
 
+  const formatPhoneNumber = (number) => {
+    number = number.toString();
+    const length = number.length;
+    if(length < 10) return number;
+    let format = `${number.substring(length-4)}`;
+    format = `${number.substring(length-4-3, length-4)} ${format}`;
+    format = `(${number.substring(length-4-3-3, length-4-3)}) ${format}`;
+    if(length > 10) {
+      format = `+${number.substring(0, length-10)} ${format}`;
+    }
+    return format;
+  };
+
   const call_time = new Date(created_at);
 
   return (
     <Card>
       <Grid
         container
+        spacing={2}
         direction="row"
         justifyContent="space-between"
         alignItems="center"
       >
         <Grid item><CallIcon call_type={call_type}/></Grid>
-        <Grid item>{to}</Grid>
+        <Grid item>{formatPhoneNumber(to)}</Grid>
         <Grid item>{getTimeOfDay(call_time)}</Grid>
       </Grid>
     </Card>
