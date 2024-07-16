a0cbac6cbe9ba465c4e309d572787d3687f446d4
Feras Muki <feras.muki@gmail.com>
2024-07-16 05:54:23 -0400

    Implement a helper that retrieve a call by id

--- src/helpers.js
+++ src/helpers.js
@@ -14,3 +14,18 @@ export async function getAllCalls() {
         console.error(error.message);
     }
 }
+
+export async function getCallById(id) {
+    try {
+        const url = `${BASE_URL}/activities/${id}`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        return json;
+    } catch(error) {
+        console.error(error.message);
+    }
+}
+
