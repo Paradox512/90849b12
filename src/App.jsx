9a73383afa513e195dc306809a26bba24f8fb9a9
Feras Muki <feras.muki@gmail.com>
2024-07-17 14:40:42 -0400

    Update the render function to use createRoot

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import Header from './Header.jsx';
 import Grid from '@mui/material/Grid';
@@ -23,6 +23,8 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App/>);
 
 export default App;
